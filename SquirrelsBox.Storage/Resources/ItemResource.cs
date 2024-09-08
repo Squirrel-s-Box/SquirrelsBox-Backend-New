69c021f4a3fc9f0ef7d6b0280cf694b51b3f574a
DiegouHZ5 <safairus@gmail.com>
2024-09-08 22:52:20 +0300

    arreglao

--- SquirrelsBox.Storage/Resources/ItemResource.cs
+++ SquirrelsBox.Storage/Resources/ItemResource.cs
@@ -45,6 +45,6 @@ namespace SquirrelsBox.Storage.Resources
         public string Name { get; set; }
         public string Description { get; set; }
         public string Amount { get; set; }
-        public string ItemPhoto { get; set; }
+        public string? ItemPhoto { get; set; }
     }
 }
